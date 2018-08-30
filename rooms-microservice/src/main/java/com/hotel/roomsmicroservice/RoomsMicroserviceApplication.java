8fa542866df8db08e3c05d011cb7ab9ecdc492d2
Your Name <dhanushkasampath.mtr@gmail.com>
2018-08-30 01:26:18 -0400

    microservices learn

---
+++ rooms-microservice/src/main/java/com/hotel/roomsmicroservice/RoomsMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.hotel.roomsmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomsMicroserviceApplication.class, args);
+	}
+}
---
+++ staff-microservice/src/main/java/com/hotel/staffmicroservice/StaffMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.hotel.staffmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaffMicroserviceApplication.class, args);
+	}
+}
